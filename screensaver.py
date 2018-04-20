6e8437f34bf58f0dea7d511f7618a5567c963dd8
Stoycho Donev <stoychod79@gmail.com>
2018-04-20 22:04:38 +0100

    Initial commit. Base template file created.

---
+++ screensaver.py
@@ -0,0 +1,23 @@
+"""
+Screen saver module for Linux
+"""
+
+
+def idle_check():
+    """Check how long the machine has been idle for. """
+    pass
+
+
+def wake_lock_check():
+    """Check for wake lock programs."""
+    pass
+
+
+def log():
+    """Write logs."""
+    pass
+
+
+def run():
+    """Run program."""
+    pass
